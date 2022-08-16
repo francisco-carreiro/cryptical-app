a8cfa77bac6931e3cb62b3808c96ee8ec5b6551c
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-08-16 05:37:21 +0000

    1 component and 2 pages created with routing working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Coininfo from "./Pages/Coininfo";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-     Crypto eye
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" component={Home} exact />
+          <Route path="/currency/:id" component={Coininfo} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
