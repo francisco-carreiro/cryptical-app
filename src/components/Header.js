9827ca08894b37dd9bced5547c75ec967048e614
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-08-16 11:02:48 +0000

    navbar on the making

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,34 @@
+import { AppBar, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography onClick={() => navigate("/")} className="logo">
+            CRYPTO EYE
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginLeft: 15,
+            }}
+          >
+            <MenuItem value={"EUR"}>EUR</MenuItem>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"THB"}>THB</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
