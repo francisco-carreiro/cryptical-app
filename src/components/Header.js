8e237fbab224684a685f7cbb2983e190a1d58bb4
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-08-16 11:11:22 +0000

    landing page starting to look like something

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { AppBar, MenuItem, Select, Toolbar, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -6,28 +7,40 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
-    <AppBar color="transparent" position="static">
-      <Container>
-        <Toolbar>
-          <Typography onClick={() => navigate("/")} className="logo">
-            CRYPTO EYE
-          </Typography>
-          <Select
-            variant="outlined"
-            style={{
-              width: 100,
-              height: 40,
-              marginLeft: 15,
-            }}
-          >
-            <MenuItem value={"EUR"}>EUR</MenuItem>
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"THB"}>THB</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              className="logo"
+              variant="h5"
+            >
+              CRYPTO EYE
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"EUR"}>EUR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"THB"}>THB</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
