d9f24213697cf9bbe366e09a15443f3df7baa699
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-08-19 17:39:56 +0000

    fixed API was not getting all available crypto currencies

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export const CryptoList = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=12963&page=1&sparkline=false`;
 
 export const SingleCrypto = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
@@ -8,4 +8,4 @@ export const HistoricalChart = (id, days = 365, currency) =>
   `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
 
 export const TrendingCrypto = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=12963&page=1&sparkline=false&price_change_percentage=24h`;
