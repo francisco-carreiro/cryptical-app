664a2269b36219c2ad9c3d89ac0062f0e82112dc
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-10-05 13:51:46 -0100

    Home page finished

--- src/components/CryptoTable.js
+++ src/components/CryptoTable.js
@@ -1,6 +1,7 @@
 import {
   Container,
   LinearProgress,
+  Pagination,
   Table,
   TableBody,
   TableCell,
@@ -23,6 +24,8 @@ const CryptoTable = () => {
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+
   const { currency, symbol } = CryptoState();
   const fetchCrypto = async () => {
     setLoading(true);
@@ -30,6 +33,7 @@ const CryptoTable = () => {
     setCrypto(data);
     setLoading(false);
   };
+
   useEffect(() => {
     fetchCrypto();
   }, [currency]);
@@ -92,73 +96,99 @@ const CryptoTable = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {handleSearch().map((row) => {
-                  const profit = row.price_change_percentage_24h > 0;
-                  return (
-                    <TableRow
-                      onClick={() => navigate("/Chart/${row.id}")}
-                      className="row"
-                      key={row.name}
-                    >
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{
-                          display: "flex",
-                          gap: 15,
-                        }}
+                {handleSearch()
+                  .slice((page - 1) * 10, (page - 1) * 10 + 10)
+                  .map((row) => {
+                    const profit = row.price_change_percentage_24h > 0;
+                    return (
+                      <TableRow
+                        onClick={() => navigate("/Chart/${row.id}")}
+                        className="row"
+                        key={row.name}
                       >
-                        <img
-                          src={row?.image}
-                          alt={row.name}
-                          height="45"
-                          style={{ marginBottom: 10 }}
-                        />
-                        <div
-                          style={{ display: "flex", flexDirection: "column" }}
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          style={{
+                            display: "flex",
+                            gap: 15,
+                          }}
                         >
-                          <span
-                            style={{ textTransform: "uppercase", fontSize: 24 }}
+                          <img
+                            src={row?.image}
+                            alt={row.name}
+                            height="45"
+                            style={{ marginBottom: 10 }}
+                          />
+                          <div
+                            style={{ display: "flex", flexDirection: "column" }}
                           >
-                            {row.symbol}
-                          </span>
-                          <span style={{ color: "darkgray" }}>{row.name}</span>
-                        </div>
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        style={{ textTransform: "uppercase", fontWeight: 800 }}
-                      >
-                        {symbol}{" "}
-                        {numberWithCommas(row.current_price.toFixed(2))}
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        style={{
-                          color: profit > 0 ? "limegreen" : "crimson",
-                          fontWeight: 800,
-                        }}
-                      >
-                        {profit && "+"}
-                        {row.price_change_percentage_24h.toFixed(2)}%
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        style={{ textTransform: "uppercase", fontWeight: 800 }}
-                      >
-                        {symbol}{" "}
-                        {numberWithCommas(
-                          row.market_cap.toString().slice(0, -6)
-                        )}{" "}
-                        M
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
+                            <span
+                              style={{
+                                textTransform: "uppercase",
+                                fontSize: 24,
+                              }}
+                            >
+                              {row.symbol}
+                            </span>
+                            <span style={{ color: "darkgray" }}>
+                              {row.name}
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            textTransform: "uppercase",
+                            fontWeight: 800,
+                          }}
+                        >
+                          {symbol}{" "}
+                          {numberWithCommas(row.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            color: profit > 0 ? "limegreen" : "crimson",
+                            fontWeight: 800,
+                          }}
+                        >
+                          {profit && "+"}
+                          {row.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            textTransform: "uppercase",
+                            fontWeight: 800,
+                          }}
+                        >
+                          {symbol}{" "}
+                          {numberWithCommas(
+                            row.market_cap.toString().slice(0, -6)
+                          )}{" "}
+                          M
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
               </TableBody>
             </Table>
           )}
         </TableContainer>
+        <Pagination
+          style={{
+            padding: 20,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          count={(handleSearch()?.length / 10).toFixed(0)}
+          onChange={(_, value) => {
+            setPage(value);
+            window.scroll(0, 450);
+          }}
+        />
       </Container>
     </ThemeProvider>
   );
