6510ade75cc36d2b3c11859069ff66e7c75ba2de
Francisco Machado Carreiro <franciscomcarreiro@gmail.com>
2022-10-05 12:40:01 -0100

    info rectification and restyling

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import Carousel from "./Couresel";
 
 import React from "react";
@@ -7,20 +7,6 @@ const Banner = () => {
   return (
     <div className="banner">
       <Container className="bannerContent">
-        <div className="tagline">
-          <img className="coinLogo" src="/coinLogo.png" alt="cryptical coin" />
-          <Typography
-            variant="subtitle1"
-            style={{
-              color: "white",
-              textTransform: "uppercase",
-              fontFamily: "Oswald, sans-serif",
-              fontWeight: 400,
-            }}
-          >
-            All the information about your cryptos in one place!
-          </Typography>
-        </div>
         <Carousel />
       </Container>
     </div>
